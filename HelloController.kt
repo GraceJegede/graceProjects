69446368ca96833f2cfdbd8b0856b81255e4bf6d
Grace Jegede <72767378+graceAJ@users.noreply.github.com>
2022-05-25 12:05:18 -0600

    first commit

---
+++ HelloController.kt
@@ -0,0 +1,14 @@
+package com.example.readimage3
+
+import javafx.fxml.FXML
+import javafx.scene.control.Label
+
+class HelloController {
+    @FXML
+    private lateinit var welcomeText: Label
+
+    @FXML
+    private fun onHelloButtonClick() {
+        welcomeText.text = "Welcome to JavaFX Application!"
+    }
+}
